9892d66a9bac51add6e06cc2980835744deb3f13
RAIMONES <raimonesrg3@gmail.com>
2023-04-07 17:51:29 -0300

    deslizar no teclado virtual

--- scripts/main.js
+++ scripts/main.js
@@ -70,14 +70,78 @@ aulas.forEach(el=>{
   teclado_virtual.showTeclado(modelo, aula, local)
 })
 
+// ARRASTAR NO TECLADO
+
+let onTouchLeaveEvents = [];
+let onTouchEnterEvents = [];
+const onTouchEnter = function (selector, fn) {
+  onTouchEnterEvents.push([selector, fn]);
+  return function () {
+    onTouchEnterEvents.slice().map(function (e, i) {
+      if (e[0] === selector && e[1] === fn) {
+        onTouchEnterEvents.splice(1, i);
+      }
+    });
+  };
+};
+
+const onTouchLeave = function (selector, fn) {
+  onTouchLeaveEvents.push([selector, fn]);
+  return function () {
+    onTouchLeaveEvents.slice().map(function (e, i) {
+      if (e[0] === selector && e[1] === fn) {
+        onTouchLeaveEvents.splice(1, i);
+      }
+    });
+  };
+};
+
+let lastTouchLeave;
+let lastTouchEnter;
+document.addEventListener('touchmove', function (e) {
+  var el = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY);
+  if (!el) return;
+  
+  onTouchLeaveEvents.map((event) => {
+    if (el!=lastTouchEnter && lastTouchEnter && lastTouchEnter.matches(event[0])) {
+      if (lastTouchEnter !== lastTouchLeave ) {
+        event[1](lastTouchEnter, e);
+        lastTouchLeave = lastTouchEnter;
+        lastTouchEnter = null
+        }
+      }
+    });
+  
+    onTouchEnterEvents.map((event) => {
+        if (el.matches(event[0]) && el!==lastTouchEnter) {
+            lastTouchEnter = el;
+      lastTouchLeave = null;
+      event[1](el, e);
+     }
+    });
+
+});
+
+onTouchEnter('button',function(el,e){
+  teclado_virtual.playAudio(el)
+  class_aulas.showNote(el);
+})
+onTouchLeave('button',function(el,e){
+  teclado_virtual.playAudio(el)
+  class_aulas.showNote(el);
+})
+
+//asdfghafsdh
+
 // Tocar com teclado virtual
 const btnKeyboard = document.querySelectorAll('.keyboard button');
-btnKeyboard.forEach(el=>{
-  el.addEventListener('click', evt=>{
-    teclado_virtual.playAudio(evt.target);
+btnKeyboard.forEach(tecla=>{
+  tecla.addEventListener('mousedown', evt=>{
     const event = evt.target;
-    class_aulas.showNote(event)
-  })
+    teclado_virtual.playAudio(event)
+    class_aulas.showNote(event);
+  });
+  
 })
 
 // Eventos de navegação
