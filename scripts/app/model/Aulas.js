e9348a920dc23b6e6ce452310073127fd2d9d21c
RAIMONES <raimonesrg3@gmail.com>
2023-04-14 17:26:49 -0300

    update Aulas.js

--- scripts/app/model/Aulas.js
+++ scripts/app/model/Aulas.js
@@ -2,8 +2,8 @@ const modelAulas = {
   notas: [ 'DÓ+ ou RÉ-', 'RÉ+ ou MÍ-', 'FÁ+ ou SOL-', 'SOL+ ou LÁ-', 'LÁ+ ou SÍ-', 'DÓ', 'RÉ', 'MÍ', 'FÁ', 'SOL', 'LÁ', 'SÍ'],
 
   dedos: [
-    ['1-E', '2-E', '3-E', '4-E', '5-E', '3-E', '4-E', '5-E ou 1-D', '2-D', '3-D', '1-D', '2-D', '3-D', '4-D'],
-    ['1-E', '2-E', '3-E', '4-E', '5-E', '3-E', '4-E', '5-E ou 1-D', '2-D', '3-D', '1-D', '2-D', '3-D', '4-D']
+    ['', '', '', '', '', '1-E', '2-E', '3-E', '4-E', '5-E', '3-E', '4-E'],
+    ['', '', '', '', '', '5-E ou 1-D', '2-D', '3-D', '1-D', '2-D', '3-D', '4-D']
   ],
 
   cifra: ['C# ou Db', 'D# ou Eb', 'F# ou Gb', 'G# ou Ab', 'A# ou Bb', 'C', 'D', 'E', 'F', 'G', 'A', 'B'],
@@ -17,11 +17,15 @@ export class Aulas{
   }
 
   learn(infoKey){
-    // console.log(infoKey)
+    console.log(infoKey)
     if(infoKey.position){
       this.aulaNotes(infoKey, 'fisico')
+      this.aulaDedos(infoKey, 'fisico')
+      this.aulaCifras(infoKey, 'fisico')
     } else{
       this.aulaNotes(infoKey, 'digital')
+      this.aulaDedos(infoKey, 'digital')
+      this.aulaCifras(infoKey, 'digital')
     }
 
   }
@@ -49,7 +53,34 @@ export class Aulas{
 
   }
 
-  
+  aulaDedos(infoKey, tipo){
+    if(tipo=='fisico'){
+      if(infoKey.parent=='dedos-teoria'){
+        let i = modelAulas.cifra.indexOf(infoKey.content)
+        if(infoKey.position<60){
+          this.showSimple(modelAulas.dedos[0][i], infoKey.parent)
+        }else{
+          this.showSimple(modelAulas.dedos[1][i], infoKey.parent)
+        }
+      }
+    }
+
+    if(tipo=='digital'){
+      if(infoKey.parent=='dedos-teoria'){
+        this.showSimple(infoKey.content, infoKey.parent)
+      }
+    }
+  }
+
+  aulaCifras(infoKey, tipo){
+      if(infoKey.parent=='cifra-teoria'){
+        this.showSimple(infoKey.content, infoKey.parent)
+      }
+      if(infoKey.parent=='cifra-pratica'){
+        this.showPratic(infoKey.content, infoKey.parent)
+      }
+
+  }
 
   showSimple(content, parent){
     let local = document.querySelector(`#${parent} h1`);
